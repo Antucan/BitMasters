f26dbe9302a580ffa7d0b66dc17442612463b74a
kaimex9 <kaimexalabau@gmail.com>
2025-02-26 17:12:49 +0100

    some styles added

--- bm_frontend/src/app/login/login.component.ts
+++ bm_frontend/src/app/login/login.component.ts
@@ -2,12 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   name: string = '';
   pass: string = '';
+
+  updateName(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.name = inputElement.value;
+  }
+
+  updatePass(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.pass = inputElement.value;
+  }
+
+  showData(): void {
+    console.log('Login:', this.name, this.pass);
+  }
 }
