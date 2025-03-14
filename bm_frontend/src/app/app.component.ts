0dd76705d5cd0fcdc9b9de40f20c8772c3318573
AlexLi727 <alexavililee@gmail.com>
2025-03-14 17:17:43 +0100

    avances de la version

--- bm_frontend/src/app/app.component.ts
+++ bm_frontend/src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RegisterComponent } from './register/register.component';
 import {RouterModule, Routes} from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginComponent, HeaderComponent, RegisterComponent, RouterModule, FooterComponent],
+  imports: [LoginComponent, HeaderComponent, RegisterComponent, RouterModule, FooterComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- bm_frontend/src/app/app.routes.ts
+++ bm_frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
