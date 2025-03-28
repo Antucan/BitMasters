61d4e0af16e79f8a1ac475780457c9c09332646a
kaimex9 <kaimexalabau@gmail.com>
2025-03-28 16:48:14 +0100

    pagination finished

--- bm_frontend/src/app/app.component.ts
+++ bm_frontend/src/app/app.component.ts
@@ -6,12 +6,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductosComponent } from './components/productos/productos.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginComponent, HeaderComponent, RegisterComponent, RouterModule, FooterComponent, ProductosComponent, HttpClientModule],
+  imports: [CommonModule, LoginComponent, HeaderComponent, RegisterComponent, RouterModule, FooterComponent, ProductosComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
