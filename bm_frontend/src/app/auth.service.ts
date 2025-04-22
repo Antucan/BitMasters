e182f2e032ce8e949304a45ddb9ca0181adb8e1f
Antucan <antherpra@gmail.com>
2025-04-22 17:13:48 +0200

    #91 using the observable user from header component

--- bm_frontend/src/app/auth.service.ts
+++ bm_frontend/src/app/auth.service.ts
@@ -2,19 +2,35 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BehaviorSubject } from 'rxjs';
+import { User } from './models/user.model'; // Update the path to the User model
+import { tap } from 'rxjs';//para actualizar la propiedad user
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private apiUrl = 'http://127.0.0.1:8000/users/login'; // Reemplaza con tu URL de Symfony
-  private clientLogged: any = null;
+  private userSubject = new BehaviorSubject<User | null>(null);
 
   constructor(private http: HttpClient) { }
+
+  //Observable para exponer los datos del usuario
+  get user$(): Observable<User | null> {
+    return this.userSubject.asObservable();
+  }
 
   login(credentials: { mail: string, password: string }): Observable<any> {
     const headers = { 'Content-Type': 'application/json' };
     console.log('Sending login request to:', this.apiUrl, 'with credentials:', credentials);
-    return this.http.post<any>(this.apiUrl, credentials, { headers });
+
+    return this.http.post<any>(this.apiUrl, credentials, { headers }).pipe(
+      tap(user => {
+        this.userSubject.next(user); // Actualiza el BehaviorSubject con el nuevo usuario
+        console.log('User logged in:', user);
+      })
+    );
+  }
+  logout(): void {
+    this.userSubject.next(null); // Limpiar el usuario al cerrar sesión
   }
 }
--- bm_frontend/src/app/components/header/header.component.ts
+++ bm_frontend/src/app/components/header/header.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { BannerComponent } from '../banner/banner.component';
-
 import { CartService } from '../cart-preview/cart.service';
-
-
-
 import { LoginService } from '../login/login.service';
+import { User } from '../../models/user.model';
+import { AuthService } from '../../auth.service';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [BannerComponent],
+  imports: [CommonModule, BannerComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  logged: boolean = false;
+  constructor(private router: Router, private cartService: CartService, private loginService: LoginService, private authService: AuthService) { }
 
-  constructor(private router: Router, private cartService: CartService, private loginService: LoginService) {}
+  ngOnInit() {
+    this.authService.user$.subscribe(user => {
+      this.logged = !!user; // Cambia a true si hay un usuario, false si no
+      console.log('User:', user, 'Logged:', this.logged);
+    });
+  }
 
   redirectTo(route: string): void {
     this.router.navigate([route]);
@@ -29,10 +35,8 @@ export class HeaderComponent {
     this.loginService.showLogin();
   }
 
- 
-
   onCartClick() {
     this.cartService.showCart(); // muestra el carrito
   }
 
-}
+ }
