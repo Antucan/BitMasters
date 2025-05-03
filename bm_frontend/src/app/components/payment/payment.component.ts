3d085d07b89b8f4db473671bf77c1f8ad449f52b
perezadria00 <perezadria00@gmail.com>
2025-05-03 18:12:22 +0200

    Create payment method component

--- bm_frontend/src/app/components/payment/payment.component.ts
+++ bm_frontend/src/app/components/payment/payment.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '../cart/cart.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
-  imports: [],
   standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
 export class PaymentComponent {
+  totalPrice: number = 0;
 
+  cardName = '';
+  cardNumber = '';
+  cardExpiry = '';
+  cardCvv = '';
+
+  constructor(private cartService: CartService, private router: Router) {
+    this.totalPrice = this.cartService.getTotalPrice();
+  }
+
+  confirmPayment() {
+    alert('Pago realizado correctamente con tarjeta');
+    this.router.navigate(['/']);
+  }
 }
