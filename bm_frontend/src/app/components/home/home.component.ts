8866f60c86a9c0c4fe2ba27363ecaefdfb13038b
{kaimex9} <{kaimexalabau@gmail.com}>
2025-05-04 02:47:22 +0200

    home css finished

--- bm_frontend/src/app/components/home/home.component.ts
+++ bm_frontend/src/app/components/home/home.component.ts
@@ -1,24 +1,40 @@
-import { Component } from '@angular/core';
-import { LoginComponent } from '../login/login.component';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
 import { LoginService } from '../login/login.service';
-
-
-
+import { ProductosService } from '../productos/productos.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LoginComponent, CommonModule],
+  imports: [LoginComponent, CommonModule, RouterModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css', '../productos/productos.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   loginVisible = false;
+  paginatedProducts: Product[] = []; // Variable para almacenar los 4 primeros productos
 
-  constructor(private loginService: LoginService) {
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private productosService: ProductosService
+  ) {
     this.loginService.loginVisible$.subscribe(visible => {
       this.loginVisible = visible;
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadProducts(); // Llama al método para cargar los productos
+  }
+
+  loadProducts(): void {
+    this.productosService.getProductos().subscribe((data: Product[]) => {
+      console.log('Productos:', data); // Muestra los productos en la consola
+      this.paginatedProducts = data.slice(0, 4); // Obtén los primeros 4 productos
     });
   }
 }
