de7ba91de70cc71b105e96495b276dee848c115a
perezadria00 <perezadria00@gmail.com>
2025-05-07 16:17:44 +0200

    #117 catalog done

--- bm_frontend/src/app/components/catalog/catalog.component.ts
+++ bm_frontend/src/app/components/catalog/catalog.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-catalog',
-  standalone: true, 
-  imports: [],
+  standalone: true,
+  imports: [CommonModule], 
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  @Output() closeModal = new EventEmitter<void>();
+  visible = false;
 
+  ngOnInit() {
+    setTimeout(() => this.visible = true, 10); 
+  }
+
+  close() {
+    this.visible = false;
+    setTimeout(() => this.closeModal.emit(), 300); 
+  }
 }
