d5898dd7b6fafcf98a631b3e9ed027825c01ef9f
Antucan <antherpra@gmail.com>
2025-05-22 18:22:06 +0200

    #201 adding purchases to admin

--- bm_frontend/src/app/components/admin/home/home.component.ts
+++ bm_frontend/src/app/components/admin/home/home.component.ts
@@ -5,32 +5,39 @@ import { ClientsComponent } from "./clients/clients.component";
 import { ProductsComponent } from "./products/products.component";
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { PurchaseService } from '../purchase.service';
+import { PurchasesComponent } from './purchases/purchases.component';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [ClientsComponent, ProductsComponent, CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, ClientsComponent, ProductsComponent, PurchasesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   countClients: number = 0;
   countProducts: number = 0;
+  countPurchases: number = 0;
   clientsComponent = false;
   productsComponent = false;
+  purchaseComponent = false;
 
-  constructor(private productService: ProductService, private userService: UserService) { }
+  constructor(private productService: ProductService, private userService: UserService, private purchaseService: PurchaseService) { }
 
   ngOnInit(): void {
     // Llama al método countClients del servicio y actualiza la propiedad countClients
     this.userService.countClients().subscribe(clients => {
       this.countClients = clients.length; // Asigna la cantidad de clientes
     });
-    // Llama al método countProducts del servicio y actualiza la propiedad countProducts
+
     this.productService.countProducts().subscribe(products => {
-      this.countProducts = products.length; // Asigna la cantidad de productos
+      this.countProducts = products.length;
     });
 
+    this.purchaseService.countPurchases().subscribe(purchases => {
+      this.countPurchases = purchases.length;
+    });
   }
 
   showDashboard() {
@@ -46,6 +53,12 @@ export class HomeComponent {
   showProducts() {
     this.clientsComponent = false;
     this.productsComponent = true;
+  }
+
+  showPurchases() {
+    this.clientsComponent = false;
+    this.productsComponent = false;
+    this.purchaseComponent = true;
   }
 }
 
